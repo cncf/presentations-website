0fbe9c642f5aeac7ffdb8f1202834fea36163b1e
Chris Abraham <cjyabraham@gmail.com>
2023-08-11 15:34:39 -0400

    temporarily import all programs
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/admin/partials/sync-programs.php
+++ web/wp-content/mu-plugins/wp-mu-plugins/lf-mu/admin/partials/sync-programs.php
@@ -35,7 +35,7 @@ foreach ( $chapters as $chapter ) {
 
 			$dt_end = strtotime( $program->end_date );
 
-			if ( $dt_end < time() - ( 14 * DAY_IN_SECONDS ) || $dt_end > time() + DAY_IN_SECONDS ) {
+			if ( $dt_end > time() + DAY_IN_SECONDS ) {
 				// avoid updating programs that ended more than 2 weeks ago to limit computation.
 				// don't import programs that haven't ended yet.
 				continue;
