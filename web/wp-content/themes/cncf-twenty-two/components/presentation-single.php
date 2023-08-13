31a2440a1744b2ca18bd33ddce882d54a4dc1e5a
Chris Abraham <cjyabraham@gmail.com>
2023-08-13 18:48:55 -0400

    Fixed up the single post view with tags etc.
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/cncf-twenty-two/components/presentation-single.php
+++ web/wp-content/themes/cncf-twenty-two/components/presentation-single.php
@@ -9,27 +9,8 @@
 
 wp_enqueue_style( 'wp-block-embed' );
 
-// Get date and time now.
-$dat_now = new DateTime( '', new DateTimeZone( 'America/Los_Angeles' ) );
-
-// Get date and time of presentation for comparison.
-$presentation_date              = get_post_meta( get_the_ID(), 'lf_presentation_date', true );
-$presentation_start_time        = get_post_meta( get_the_ID(), 'lf_presentation_start_time', true );
-$presentation_start_time_period = get_post_meta( get_the_ID(), 'lf_presentation_start_time_period', true );
-$presentation_end_time          = get_post_meta( get_the_ID(), 'lf_presentation_end_time', true );
-$presentation_end_time_period   = get_post_meta( get_the_ID(), 'lf_presentation_end_time_period', true );
-$presentation_timezone          = get_post_meta( get_the_ID(), 'lf_presentation_timezone', true );
-$dat_presentation_start         = Lf_Utils::get_presentation_date_time( $presentation_date, $presentation_start_time, $presentation_start_time_period, $presentation_timezone );
-$dat_presentation_end           = Lf_Utils::get_presentation_date_time( $presentation_date, $presentation_end_time, $presentation_end_time_period, $presentation_timezone );
-
-// Get the timezone this way since we lose case otherwise and the Google cal entry won't work.
-$tzlist = DateTimeZone::listIdentifiers( DateTimeZone::ALL );
-$tzs    = array();
-foreach ( $tzlist as $tz ) {
-	$slug         = strtolower( str_replace( '/', '-', $tz ) );
-	$tzs[ $slug ] = $tz;
-}
-$dat_presentation_start_tz = $tzs[ $presentation_timezone ];
+$presentation_date  = new DateTime( get_post_meta( get_the_ID(), 'lf_presentation_date', true ) );
+$tags               = get_the_terms( get_the_ID(), 'lf-presentation-tags' );
 
 // get recording URL.
 $recording_url = get_post_meta( get_the_ID(), 'lf_presentation_recording_url', true );
@@ -49,64 +30,26 @@ $slides_url = get_post_meta( get_the_ID(), 'lf_presentation_slides_url', true );
 // get presentation views.
 $presentation_views = get_post_meta( get_the_ID(), 'lf_presentation_recording_views', true );
 
-// date period.
-if ( $dat_presentation_end > $dat_now ) {
-	$period_status = 'upcoming';
-} elseif ( ( $dat_presentation_end < $dat_now ) && ( $recording_url ) ) {
-	$period_status = 'recorded';
-} else {
-	$period_status = 'past';
-}
-
+$period_status = 'recorded';
 ?>
 <main class="presentation-single">
 	<article class="container wrap">
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
-			if ( 'upcoming' === $period_status ) :
-				// Upcoming state added just in case URL is visible.
-				?>
-		<p>This presentation is upcoming but we don't have the registration URL - it's details may have updated. <a href="https://community.cncf.io/events/#/list">Visit CNCF Community Groups</a> to find out more about it.</p>
-				<?php
-		else :
 			?>
-
-			<?php
-			// the company - presented by.
-			if ( $company ) :
-				?>
-		<div class="presentation-single__company">Presented by:
-				<?php echo esc_html( $company ); ?></div>
-
-		<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
-
-		<?php endif; ?>
 
 		<div class="presentation-single__meta-wrapper">
 
-				<?php
-				if ( is_a( $dat_presentation_start, 'DateTime' ) ) :
-					?>
 
 			<div class="presentation-single__date">
+					<img width="18" height="14" src="<?php LF_utils::get_svg( 'icon-camera.svg', true ); ?>" alt="Camera Icon" class="presentation-single__svg"> Recorded:
 					<?php
-					if ( 'recorded' === $period_status ) {
-						?>
-							<img width="18" height="14" src="<?php LF_utils::get_svg( 'icon-camera.svg', true ); ?>" alt="Camera Icon" class="presentation-single__svg"> Recorded:
-						<?php
-					} else {
-						?>
-				Broadcast:
-						<?php
-					}
-					echo esc_html( $dat_presentation_start->format( 'l F j, Y' ) );
+					echo esc_html( $presentation_date->format( 'F j, Y' ) );
 					?>
 			</div>
 
 					<?php
-				endif;
 				if ( $presentation_views ) :
 					?>
 			<div class="presentation-single__views">
@@ -155,7 +98,6 @@ if ( $dat_presentation_end > $dat_now ) {
 
 		<div style="height:80px" aria-hidden="true" class="wp-block-spacer is-style-40-80">
 		</div>
-
 		<?php endif; ?>
 
 		<div class="post-content">
@@ -165,11 +107,28 @@ if ( $dat_presentation_end > $dat_now ) {
 		</div>
 
 			<?php
-		endif;
 endwhile;
 		?>
-		<div style="height:80px"
-			aria-hidden="true" class="wp-block-spacer is-style-60-100">
+		<div aria-hidden="true" class="wp-block-spacer is-style-40-60">
+		</div>
+
+		<ul class="presentation-recorded-item__tags">
+			<?php 
+			foreach( $tags as $tag ) {
+				$tag_link = '?_sft_lf-presentation-tags=' . $tag->slug;
+				?>
+				<li>
+				<a 	class="tag"
+					title="See <?php echo esc_attr( $tag->name ); ?> presentations"
+					href="<?php echo esc_url( $tag_link ); ?>">
+					<?php echo esc_html( $tag->name ); ?></a>
+				</li>
+				<?php
+			}
+			?>
+		</ul>
+
+		<div aria-hidden="true" class="wp-block-spacer is-style-40-60">
 		</div>
 		<?php
 		get_template_part( 'components/social-share' );
